f309edd77b347783b2de86b9b8affe4354d0af68
msa <msai.sadehi@gmail.com>
2023-06-28 14:58:13 +0330

    first

---
+++ first.py
@@ -0,0 +1,40 @@
+# score = 100
+# message = "I scored score"
+# message = f"I scored {score}"
+# print(message)
+
+# name = input('Enter your name:> ')
+# family = input("Enter your family:> ")
+# message = f"Hello {name} {family}"
+# print(message)
+
+# name = input("enter a name:> ")
+# number = int(input("how many times do you want to repeat? "))
+
+# print((name + " " ) * number)
+
+# TODO   برنامه ای بنویسید که نام، سن و قد یک دانش اموز را از ورودی دریافت نماید 
+# و سپس مانند متن زیر در خزوجی نمایش دهد
+# سه متغیر نیاز دارید
+# arman has 12 years old and his height is 167 cm.
+
+
+# name = "arshin"
+# print(name[0])
+
+# TODO   برنامه ای بنویسید که نام کاربر را از ووردی دریافت نماید و کاراکترهای آن را جداجدا پرینت نماید.
+
+# numbers = [1,22,0,4,84]
+# print(numbers[0])
+# print(numbers[1])
+
+# TODO  یک لیست از اسامی پنج دانش آموز ایجاد کنید و مثل خطوط 28 و 29 اعضای لیست را نمایش دهید
+
+# numbers = [1,2,3,4,5,6,7,8,9]
+# print(numbers[:3])
+# print(numbers[3:6])
+
+fruits = ["apple", "bannana","watermelon","melon"]
+print(fruits[:2])
+print(fruits[1:3])
+print(fruits[2:])
