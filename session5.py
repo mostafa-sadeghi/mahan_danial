a68536f09c4dfec5b4890b9c8214cda310373563
msa <msai.sadehi@gmail.com>
2023-07-26 14:58:46 +0330

    turtle

---
+++ session5.py
@@ -0,0 +1,34 @@
+import turtle
+
+window = turtle.Screen()
+window.title("hello")
+window.bgcolor('orange')
+# window.bgpic("flappyBackground.png")
+
+
+my_turtle = turtle.Turtle()
+my_turtle.shape("turtle")  # 'arrow', 'turtle', 'circle', 'square', 'triangle', 'classic'
+
+my_turtle.forward(150)
+my_turtle.left(120)
+my_turtle.forward(150)
+my_turtle.left(120)
+my_turtle.forward(150)
+my_turtle.left(120)
+
+# TODO
+
+"""
+تمرین:
+کشیدن مربع،
+ پنجضلعی
+شش ضلعی
+هفت  ضلعی
+هشت ضلعی
+نه ضلعی
+
+"""
+
+
+
+turtle.done()
